fbe930692886809fff18682bf4cbe9f95e3d86c0
burpOverflow <code.mrsp@gmail.com>
2021-08-11 21:17:11 +0530

    bug fixed

--- cmd/root.go
+++ cmd/root.go
@@ -16,14 +16,10 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
-	"os"
 	"sync"
 
 	"github.com/burpOverflow/goRecz/pkg/banner"
 	"github.com/spf13/cobra"
-
-	"github.com/spf13/viper"
 )
 
 var (
@@ -52,40 +48,40 @@ func Execute() {
 	cobra.CheckErr(rootCmd.Execute())
 }
 
-func init() {
-	cobra.OnInitialize(initConfig)
+// func init() {
+// 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+// 	// Here you will define your flags and configuration settings.
+// 	// Cobra supports persistent flags, which, if defined here,
+// 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.goRecz.yaml)")
+// 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.goRecz.yaml)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
+// 	// Cobra also supports local flags, which will only run
+// 	// when this action is called directly.
+// 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+// }
 
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
+// // initConfig reads in config file and ENV variables if set.
+// func initConfig() {
+// 	if cfgFile != "" {
+// 		// Use config file from the flag.
+// 		viper.SetConfigFile(cfgFile)
+// 	} else {
+// 		// Find home directory.
+// 		home, err := os.UserHomeDir()
+// 		cobra.CheckErr(err)
 
-		// Search config in home directory with name ".goRecz" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".goRecz")
-	}
+// 		// Search config in home directory with name ".goRecz" (without extension).
+// 		viper.AddConfigPath(home)
+// 		viper.SetConfigType("yaml")
+// 		viper.SetConfigName(".goRecz")
+// 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
+// 	viper.AutomaticEnv() // read in environment variables that match
 
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
-}
+// 	// If a config file is found, read it in.
+// 	if err := viper.ReadInConfig(); err == nil {
+// 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+// 	}
+// }
