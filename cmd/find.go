eb71d1bfc56c9e0bccdbc75e88d52f63f27e0fee
burpOverflow <code.mrsp@gmail.com>
2021-08-11 10:54:02 +0530

    cobra remake

---
+++ cmd/find.go
@@ -0,0 +1,57 @@
+/*
+Copyright © 2021 burpOverflow
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/burpOverflow/goRecz/find"
+	"github.com/spf13/cobra"
+)
+
+// findCmd represents the find command
+var findCmd = &cobra.Command{
+	Use:   "find",
+	Short: "Find subdomains (Subdomain Enumeration) ",
+
+	Run: findHandler,
+}
+
+func init() {
+	rootCmd.AddCommand(findCmd)
+
+	findCmd.Flags().StringP("domain", "d", "", "domain name")
+	findCmd.Flags().StringP("mode", "m", "passive", "passive mode or active mode")
+	findCmd.Flags().BoolP("src", "s", false, "show data source")
+
+	findCmd.MarkFlagRequired("domain")
+}
+
+func findHandler(cmd *cobra.Command, args []string) {
+	domain, _ := cmd.Flags().GetString("domain")
+	mode, _ := cmd.Flags().GetString("mode")
+	src, _ := cmd.Flags().GetBool("src")
+
+	if mode == "passive" {
+		find.PassiveHandler(&domain, &src, wg)
+	}
+	if mode == "active" {
+		fmt.Println("Active Scan Not available for Now :)")
+		os.Exit(0)
+	}
+	wg.Wait()
+}
